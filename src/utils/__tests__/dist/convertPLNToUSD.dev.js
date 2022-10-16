1fe274f059831264c2daa9a5757448d7d07e33fb
Marcin Grajdek <grajdek.m@vp.pl>
2022-10-16 19:55:19 +0200

    Add test unit - 21.2 orginal

--- src/utils/__tests__/dist/convertPLNToUSD.dev.js
+++ src/utils/__tests__/dist/convertPLNToUSD.dev.js
@@ -1,31 +1,31 @@
 "use strict";
 
-var _convertPLNtoUSD = require("./../convertPLNtoUSD");
+var _convertPLNToUSD = require("../../convertPLNToUSD");
 
-describe('ConvertPLNtoUSD', function () {
+describe('ConvertPLNToUSD', function () {
   it('should return proper value when good input', function () {
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(1)).toBe('$0.29');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(2)).toBe('$0.57');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(20)).toBe('$5.71');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(12)).toBe('$3.43');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(1)).toBe('$0.29');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(2)).toBe('$0.57');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(20)).toBe('$5.71');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(12)).toBe('$3.43');
   });
   it('should return NaN when input is text', function () {
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)('6')).toBeNaN();
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)('abc')).toBeNaN();
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)('-543')).toBeNaN();
+    expect((0, _convertPLNToUSD.convertPLNToUSD)('6')).toBeNaN();
+    expect((0, _convertPLNToUSD.convertPLNToUSD)('abc')).toBeNaN();
+    expect((0, _convertPLNToUSD.convertPLNToUSD)('-543')).toBeNaN();
   });
   it('should return NaN when input is empty', function () {
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)()).toBeNaN();
+    expect((0, _convertPLNToUSD.convertPLNToUSD)()).toBeNaN();
   });
   it('should return "Error" when input is different than number and string', function () {
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)({})).toBe('Error');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)([])).toBe('Error');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(null)).toBe('Error');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(function () {})).toBe('Error');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)({})).toBe('Error');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)([])).toBe('Error');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(null)).toBe('Error');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(function () {})).toBe('Error');
   });
   it('should return zero when input is lower than zero', function () {
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(-1)).toBe('$0.00');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(-2)).toBe('$0.00');
-    expect((0, _convertPLNtoUSD.convertPLNToUSD)(-56)).toBe('$0.00');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(-1)).toBe('$0.00');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(-2)).toBe('$0.00');
+    expect((0, _convertPLNToUSD.convertPLNToUSD)(-56)).toBe('$0.00');
   });
 });
